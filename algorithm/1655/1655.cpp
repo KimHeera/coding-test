1b12b0c67ea0793cc844ebdb773b1c40034c47a4
kimheera <hrk0295@naver.com>
2024-01-24 23:08:32 +0900

    코드 완성

--- algorithm/1655/1655.cpp
+++ algorithm/1655/1655.cpp
@@ -9,8 +9,12 @@ priority_queue<int> maxHeap;
 priority_queue<int, vector<int>, greater<int>> minHeap;
 bool flag = true;
 
-void main()
+int main()
 {
+    // 입출력 속도 향상을 위한 코드
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
     int inputSize;
     int inputNum;
 
@@ -18,10 +22,39 @@ void main()
     for (int i = 0; i < inputSize; i++)
     {
         cin >> inputNum;
-        CalculateHeap(inputNum);
-    }
-}
+        // 처음 입력받는 경우
+        if (maxHeap.empty())
+        {
+            maxHeap.push(inputNum);
+        }
+        else
+        {
+            // maxheap의 size가 더 크다면, minheap에 inputNum을 넣는다.
+            if (maxHeap.size() > minHeap.size())
+            {
+                minHeap.push(inputNum);
+            }
+            else
+            { // size가 같다면 maxheap에 innputNum을 넣는다.
+                maxHeap.push(inputNum);
+            }
 
-void CalculateHeap(int inputNum)
-{
+            // maxheap의 root 노드 > minheap의 root 노드 -> swap함.
+            if (maxHeap.top() > minHeap.top())
+            {
+                int maxTop = maxHeap.top();
+                int minTop = minHeap.top();
+
+                maxHeap.pop();
+                minHeap.pop();
+
+                maxHeap.push(minTop);
+                minHeap.push(maxTop);
+            }
+        }
+
+        cout << maxHeap.top() << '\n';
+    }
+
+    return 0;
 }
