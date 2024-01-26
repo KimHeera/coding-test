f2865b3222ceb73a38506a285b571eb23c821212
kimheera <hrk0295@naver.com>
2024-01-26 19:45:07 +0900

    바이러스 문제 푸는 중

--- algorithm/bfs/2606.cpp
+++ algorithm/bfs/2606.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
+
+#define MAXNUM 100
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+
+    int computerNum, connnectNum;
+    bool flag = true;
+    vector<int> network;
+
+    cin >> computerNum;
+    cin >> connnectNum;
+
+    int node1, node2;
+
+    for (int i = 0; i < connnectNum; i++)
+    {
+        cin >> node1 >> node2;
+
+        if (flag)
+        {
+            network.push_back(node1);
+            network.push_back(node2);
+        }
+        else
+        {
+                }
+    }
 }
