c553fceeabe2623fbae92e3c3b6fb168fd37932f
kimheera <hrk0295@naver.com>
2024-02-05 22:11:50 +0900

    백준 11048 풀기 시작

--- algorithm/bfs/2606.cpp
+++ algorithm/bfs/2606.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <queue>
 
 using namespace std;
 
-#define MAXNUM 100
+#define MAXNUM 101
+
+int network[MAXNUM][MAXNUM];
+int computerNum;
+int cnt = 0;
+bool visited[MAXNUM] = {
+    0,
+};
+queue<int> que;
+
+void bfs(int node)
+{
+    que.push(node);
+    visited[node] = true;
+
+    while(!que.empty()){
+        node = que.front();
+        que.pop();
+
+        for (int i = 1; i <= computerNum; i++)
+        {
+            if (network[node][i] && !visited[i] == 0)
+            {
+                que.push(i);
+                visited[i] = true;
+                cnt++;
+            }
+        }
+    }
+}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int computerNum, connnectNum;
-    bool flag = true;
-    vector<int> network;
+    int connnectNum;
 
     cin >> computerNum;
     cin >> connnectNum;
@@ -24,13 +50,11 @@ int main()
     {
         cin >> node1 >> node2;
 
-        if (flag)
-        {
-            network.push_back(node1);
-            network.push_back(node2);
-        }
-        else
-        {
-                }
+        network[node1][node2] = network[node2][node1] = 1;
     }
+
+    bfs(1);
+
+    cout << cnt;
+    return 0;
 }
