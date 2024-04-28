532c6333a651152c3b7c984437882f0305ee012c
kimheera <hrk0295@naver.com>
2024-04-28 20:00:46 +0900

    2024.04.28 이전

--- coding_tests/BOJ/2292.cpp
+++ coding_tests/BOJ/2292.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+#define SIX 6
 
 int main(){
     ios_base::sync_with_stdio(0);
@@ -9,6 +10,14 @@ int main(){
     int N;
     cin >> N;
 
-    int cnt = 1;
-    
+    int cnt = 0;
+
+    for (int max = 2; max <= N; cnt++){
+        max += cnt * SIX;
+    }
+
+    if(N == 1)
+        cnt = 1;
+
+    cout << cnt;
 }
--- coding_tests/BOJ/5430.cpp
+++ coding_tests/BOJ/5430.cpp
@@ -1,80 +1,153 @@
-/*
-
-*/
 #include <iostream>
-#include <queue>
-#include <list>
-#include <sstream>
-#include <vector>
-
+#include <deque>
+#include <algorithm>
+#include <string>
 using namespace std;
-
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int T, n;
-    int R, D;
-    string p, x;
-
-    cin >> T; //input test case
-
-    while(T--){
-        R = 0, D = 0;
-        list<int> numList;
-        int tmp;
-        cin >> p >> n >> x;
-
-        x = x.substr(1, x.length() - 2);
-
-        stringstream ss(x);
-        string token;
-        while (getline(ss, token, ','))
+int t, n;
+string strp, strn;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> t;
+    while (t--)
+    {
+        cin >> strp;
+        cin >> n;
+        cin >> strn;
+        deque<int> dq;
+        bool flag = true; // error
+        bool dir = true;  // 정방향 역방향
+        string temp = "";
+        for (int i = 1; i < strn.size(); i++) // string to deque
         {
-            tmp = stoi(token);
-            numList.push_back(tmp);
+            if (strn[i] == ',' || strn[i] == ']')
+            {
+                if (temp != "")
+                {
+                    dq.push_back(stoi(temp));
+                    temp = "";
+                }
+            }
+            else
+            {
+                temp += strn[i];
+            }
         }
-
-        //numList에 숫자가 잘 들어갔는지 확인하는 코드
-        list<int>::iterator iter;
-        // for (iter = numList.begin(); iter != numList.end(); iter++)
-        // {
-        //     cout << "Iterator : " << *iter << endl;
-        // }
-
-        for (char ch : p)
+        for (int i = 0; i < strp.size(); i++)
         {
-            if (ch == 'R')
-                R++;
-            if (ch == 'D')
-                D++;
+            if (strp[i] == 'R')
+            {
+                dir = !dir;
+            }
+            else if (strp[i] == 'D')
+            {
+                if (dq.empty())
+                {
+                    flag = false;
+                    break;
+                }
+                else // 출력
+                {
+                    if (dir)
+                        dq.pop_front();
+                    else
+                        dq.pop_back();
+                }
+            }
         }
-
-        if(D > n){
+        if (!flag)
             cout << "error\n";
-            break;
-        }
-        else{
-            for(char ch : p){
-                if(ch == 'D'){
-                    numList.pop_front();
-                }
-                else if(ch == 'R'){
-                    numList.reverse();
-                }
-            }
-        }
-
-        cout << "[";
-        for (iter = numList.begin(); iter != numList.end(); iter++)
+        else
         {
-            if (iter == numList.end()){
-                cout << *iter;
-            }else{
-                cout << *iter << ",";
+            if (!dir)
+                reverse(dq.begin(), dq.end());
+            cout << "[";
+            for (int i = 0; i < dq.size(); i++)
+            {
+                if (i == dq.size() - 1)
+                    cout << dq[i];
+                else
+                    cout << dq[i] << ",";
             }
+            cout << "]\n";
         }
-        cout << "]\n";
     }
 }
+              // #include <iostream>
+              // #include <queue>
+              // #include <list>
+              // #include <sstream>
+              // #include <vector>
 
+              // using namespace std;
+
+              // int main(){
+              //     ios_base::sync_with_stdio(0);
+              //     cin.tie(0);
+
+              //     int T, n;
+              //     int R, D;
+              //     string p, x;
+
+              //     cin >> T; //input test case
+
+              //     while(T--){
+              //         R = 0, D = 0;
+              //         list<int> numList;
+              //         int tmp;
+              //         cin >> p >> n >> x;
+
+              //         x = x.substr(1, x.length() - 2);
+
+              //         stringstream ss(x);
+              //         string token;
+              //         while (getline(ss, token, ','))
+              //         {
+              //             tmp = stoi(token);
+              //             numList.push_back(tmp);
+              //         }
+
+              //         //numList에 숫자가 잘 들어갔는지 확인하는 코드
+              //         list<int>::iterator iter;
+              //         // for (iter = numList.begin(); iter != numList.end(); iter++)
+              //         // {
+              //         //     cout << "Iterator : " << *iter << endl;
+              //         // }
+
+              //         for (char ch : p)
+              //         {
+              //             if (ch == 'R')
+              //                 R++;
+              //             if (ch == 'D')
+              //                 D++;
+              //         }
+
+              //         if(D > n){
+              //             cout << "error\n";
+              //             break;
+              //         }
+              //         else{
+              //             for(char ch : p){
+              //                 if(ch == 'D'){
+              //                     numList.pop_front();
+              //                 }
+              //                 else if(ch == 'R'){
+              //                     numList.reverse();
+              //                 }
+              //             }
+              //         }
+
+              //         cout << "[";
+              //         for (iter = numList.begin(); iter != numList.end(); iter++)
+              //         {
+              //             if (iter == numList.end()){
+              //                 cout << *iter;
+              //             }else{
+              //                 cout << *iter << ",";
+              //             }
+              //         }
+              //         cout << "]\n";
+              //     }
+              // }
