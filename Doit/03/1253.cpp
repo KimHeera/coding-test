a3f117dd5c3729e5d546b1204905d4f10dbcfbd7
kimheera <hrk0295@naver.com>
2024-07-09 21:51:03 +0900

    1253.cpp 풀이 완료

--- Doit/03/1253.cpp
+++ Doit/03/1253.cpp
@@ -16,5 +16,39 @@ int main(){
     cin.tie(0);
     cout.tie(0);
 
-    
+    int N;
+    cin >> N;
+
+    vector<int> A(N, 0);
+
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+    }
+    sort(A.begin(), A.end());
+
+    int count = 0;
+    for (int k = 0; k < N; k++)
+    {
+        long find = A[k];
+        int i = 0, j = N - 1;
+
+        while(i < j){
+            if(A[i] + A[j] == find){
+                if(i != k && j !=k){
+                    count++;
+                    break;
+                }
+                else if(i == k)
+                    i++;
+                else
+                    j--;
+            }
+            else if(A[i] + A[j] < find)
+                i++;
+            else
+                j--;
+        }
+    }
+
+    cout << count << endl;
 }
