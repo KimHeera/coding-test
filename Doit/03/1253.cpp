c91648da5f38b0d056664a731062850bccdaa6fd
kimheera <hrk0295@naver.com>
2024-07-09 21:27:24 +0900

    1253.cpp

---
+++ Doit/03/1253.cpp
@@ -0,0 +1,20 @@
+/*
+BOJ 1253.
+Author: Heera Kim
+E-mail Address: heeerala.a@gmail.com
+C++
+Last Changed: 2024.07.09
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    
+}
