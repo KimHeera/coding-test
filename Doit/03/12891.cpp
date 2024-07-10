5aa959aaf0c7eb4efcdbe793d854627a78e00cc2
kimheera <hrk0295@naver.com>
2024-07-10 19:24:19 +0900

    12891.cpp 풀이 시작

---
+++ Doit/03/12891.cpp
@@ -0,0 +1,18 @@
+/*
+BOJ 12891.
+Author: Heera Kim
+E-mail Address: heeerala.a@gmail.com
+C++
+Last Changed: 2024.07.10
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+}
