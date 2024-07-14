3feee6c290c7179f2aa19036fee8f1bf72d725d9
kimheera <hrk0295@naver.com>
2024-07-14 17:49:48 +0900

    11003 풀이 시작. deque 사용

---
+++ Doit/03/11003.cpp
@@ -0,0 +1,19 @@
+/*
+BOJ 11003.
+Author: Heera Kim
+E-mail Address: heeerala.a@gmail.com
+C++
+Last Changed: 2024.07.14
+*/
+
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    
+}
