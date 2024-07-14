057e5d2f62f1a0786a1d896500e4d27497279ac7
kimheera <hrk0295@naver.com>
2024-07-14 21:46:51 +0900

    11003.cpp 풀이 완료. 다시 풀기 필요

--- Doit/03/11003.cpp
+++ Doit/03/11003.cpp
@@ -10,10 +10,30 @@ Last Changed: 2024.07.14
 #include <deque>
 using namespace std;
 
+typedef pair<int, int> Node;
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
-    
+    int N, L;
+    deque<Node> mydeque;
+
+    cin >> N >> L;
+
+    for (int i = 0; i < N; i++){
+        int now;
+        cin >> now;
+
+        while(mydeque.size() && mydeque.back().first > now)
+            mydeque.pop_back();
+
+        mydeque.push_back(Node(now, i));
+
+        if(mydeque.front().second <= i - L)
+            mydeque.pop_front();
+
+        cout << mydeque.front().first << " ";
+    }
 }
