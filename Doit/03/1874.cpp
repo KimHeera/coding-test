9bcb8f3cb86bde569e0e338349f2794b20469159
kimheera <hrk0295@naver.com>
2024-07-14 22:23:26 +0900

    1874.cpp 풀이 시작

---
+++ Doit/03/1874.cpp
@@ -0,0 +1,23 @@
+/*
+BOJ 1874.
+Author: Heera Kim
+E-mail Address: heeerala.a@gmail.com
+C++
+Last Changed: 2024.07.14
+*/
+
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    
+}
