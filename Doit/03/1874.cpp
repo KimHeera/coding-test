2848714d3323edadb40a9b16b82cc03b0c1f6ac5
kimheera <hrk0295@naver.com>
2024-07-15 14:41:25 +0900

    1874.cpp local에서 실행 시 잘 됨. 제출하면 틀렸다고 뜸

--- Doit/03/1874.cpp
+++ Doit/03/1874.cpp
@@ -19,5 +19,46 @@ int main(){
     int N;
     cin >> N;
 
-    
+    vector<int> inputs(N, 0);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> inputs[i];
+    }
+
+    int num = 1;
+    vector<char> result;
+    stack<int> mystack;
+    bool flag = true;
+
+    for (int i = 0; i < N; i++){
+        int nowNum = inputs[i];
+
+        if(nowNum >= num){
+            while (nowNum >= num){
+                mystack.push(num);
+                num++;
+                result.push_back('+');
+            }
+            mystack.pop();
+            result.push_back('-');
+        }
+        else{
+            int n = mystack.top();
+            mystack.pop();
+
+            if(n > nowNum){
+                cout << "No";
+                flag = false;
+                break;
+            }
+            else
+                result.push_back('-');
+        }
+    }
+
+    if(flag){
+        for (int i = 0; i < result.size(); i++){
+            cout << result[i] << "\n";
+        }
+    }
 }
