c5c54a12bac64e5da14baf6dbef54343f121b597
kimheera <hrk0295@naver.com>
2024-07-20 23:13:06 +0900

    11286.cpp 풀이 시작

---
+++ Doit/03/11286.cpp
@@ -0,0 +1,33 @@
+/*
+BOJ 11286.
+Author: Heera Kim
+E-mail Address: heeerala.a@gmail.com
+C++
+Last Changed: 2024.07.20
+*/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    int x;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> x;
+        if(x == 0){
+            //출력
+        }
+        else{
+            //queue에 push
+            
+        }
+    }
+}
