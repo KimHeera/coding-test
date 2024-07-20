bd5174a63d7bc111e8c1a3a3f66fffbb03f4112e
kimheera <hrk0295@naver.com>
2024-07-20 22:59:23 +0900

    2164.cpp 풀이 완료

--- Doit/03/2164.cpp
+++ Doit/03/2164.cpp
@@ -7,6 +7,7 @@ Last Changed: 2024.07.20
 */
 
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int main(){
@@ -14,5 +15,24 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    
+    int N;
+    cin >> N;
+
+    queue<int> myQueue;
+    for (int i = 1; i <= N; i++){
+        myQueue.push(i);
+        
+    }
+
+    int top;
+    while (myQueue.size() != 1)
+    {
+        myQueue.pop();
+
+        top = myQueue.front();
+        myQueue.pop();
+        myQueue.push(top);
+    }
+
+    cout << myQueue.front() << endl;
 }
