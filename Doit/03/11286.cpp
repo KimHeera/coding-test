65990038e043801271bce0d20a93f50c000a5f8d
kimheera <hrk0295@naver.com>
2024-07-22 17:25:28 +0900

    11286.cpp 풀이 완료

--- Doit/03/11286.cpp
+++ Doit/03/11286.cpp
@@ -10,6 +10,18 @@ Last Changed: 2024.07.20
 #include <queue>
 using namespace std;
 
+struct compare{
+    bool operator()(int o1, int o2){
+        int first_abs = abs(o1);
+        int second_abs = abs(o2);
+
+        if(first_abs == second_abs)
+            return o1 > o2; //절댓값 같을 시 음수 우선 정렬
+        else
+            return first_abs > second_abs;
+    }
+};
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -17,6 +29,7 @@ int main(){
 
     int N;
     cin >> N;
+    priority_queue<int, vector<int>, compare> myQueue;
 
     int x;
     for (int i = 0; i < N; i++)
@@ -24,10 +37,16 @@ int main(){
         cin >> x;
         if(x == 0){
             //출력
+            if(myQueue.empty())
+                cout << "0\n";
+            else{
+                cout << myQueue.top() << "\n";
+                myQueue.pop();
+            }
         }
         else{
             //queue에 push
-            
+            myQueue.push(x);
         }
     }
 }
