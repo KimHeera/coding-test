93c19a8de1d0e95af5738d92783860d9f699c27a
kimheera <hrk0295@naver.com>
2024-07-25 18:05:27 +0900

    10989.cpp 풀이 완료

--- Doit/04/10989.cpp
+++ Doit/04/10989.cpp
@@ -17,5 +17,19 @@ int main(){
     int N;
     cin >> N;
 
-    
+    int count[10001] = {0};
+    int number = 0;
+
+    for (int i = 1; i < N+1; i++){
+        cin >> number;
+        count[number]++;
+    }
+
+    for (int i = 0; i <= 10000; i++){
+        if(count[i] != 0){
+            for (int j = 0; j < count[i]; j++){
+                cout << i << "\n";
+            }
+        }
+    }
 }
