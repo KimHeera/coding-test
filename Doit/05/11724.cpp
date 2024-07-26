314deadd6db69cfc7c7e7df53d4b41574c37eb4e
kimheera <hrk0295@naver.com>
2024-07-26 20:15:42 +0900

    11724.cpp 풀이 완료

--- Doit/05/11724.cpp
+++ Doit/05/11724.cpp
@@ -7,8 +7,12 @@ Last Changed: 2024.07.26
 */
 
 #include <iostream>
-#include <stack>
+#include <vector>
 using namespace std;
+
+void DFS(int v);
+vector<vector<int>> A;
+vector<bool> visited;
 
 int main(){
     ios::sync_with_stdio(false);
@@ -18,5 +22,36 @@ int main(){
     int N, M;
     cin >> N >> M;
 
-    
+    A.resize(N + 1);
+    visited = vector<bool>(N+1, false);
+
+    for (int i = 0; i < M; i++){
+        int start, end;
+        cin >> start >> end;
+
+        A[start].push_back(end);
+        A[end].push_back(start);
+    }
+
+    int result = 0;
+    for (int i = 1; i < N+1; i++)
+    {
+        if(!visited[i]){
+            result++;
+            DFS(i);
+        }
+    }
+
+    cout << result << endl;
+}
+
+void DFS(int v){
+    if(visited[v])
+        return;
+        
+    visited[v] = true;
+    for (int i = 0; i < A[v].size(); i++){
+        if(!visited[A[v][i]])
+            DFS(A[v][i]);
+    }
 }
