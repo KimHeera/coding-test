9bfbc6e619aa61a0809edff5eba885d9f1027414
kimheera <hrk0295@naver.com>
2024-07-29 18:35:29 +0900

    BOJ 10845.cpp 풀이 시작

---
+++ coding_tests/BOJ/10845.cpp
@@ -0,0 +1,19 @@
+/*
+BOJ 10845.
+Author: Heera Kim
+E-mail Address: heeerala.a@gmail.com
+C++
+Last Changed: 2024.07.29
+*/
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    
+}
