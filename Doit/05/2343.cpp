b2074b867a00702ce8b7767a15f22e717da6c1a9
kimheera <hrk0295@naver.com>
2024-08-03 18:53:55 +0900

    2343.cpp 풀이 완료 다시 풀어봐야 함

--- Doit/05/2343.cpp
+++ Doit/05/2343.cpp
@@ -6,3 +6,50 @@ C++
 Last Changed: 2024.08.03
 */
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> A(N, 0);
+    int start = 0, end = 0;
+    for (int i = 0; i < N; i++){
+        cin >> A[i];
+
+        if(start < A[i])
+            start = A[i];
+
+        end += A[i];
+    }
+
+    while(start <= end){
+        int mid = (start + end) / 2;
+        int sum = 0, count = 0;
+
+        for (int i = 0; i < N; i++){
+            if(sum + A[i] > mid){
+                count++;
+                sum = 0;
+            }
+
+            sum += A[i];
+        }
+
+        if(sum != 0)
+            count++;
+        
+        if(count > M)
+            start = mid + 1;
+        else
+            end = mid - 1;
+    }
+
+    cout << start << endl;
+}
