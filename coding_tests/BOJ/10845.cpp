0862c0eba814c032f9cfaf081d33e49ac79039bd
kimheera <hrk0295@naver.com>
2024-08-03 15:55:59 +0900

    1920.cpp 풀이 시작

--- coding_tests/BOJ/10845.cpp
+++ coding_tests/BOJ/10845.cpp
@@ -10,10 +10,62 @@ Last Changed: 2024.07.29
 #include <queue>
 using namespace std;
 
+void condition(string s);
+queue<int> myQ;
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++){
+        string s;
+        cin >> s;
+
+        condition(s);
+    }
+}
+
+void condition(string s){
+    if(s == "push"){
+        int num;
+        cin >> num;
+        myQ.push(num);
+    }
+    else if (s == "pop"){
+        if(myQ.empty())
+            cout << -1 << "\n";
+        else{
+            cout << myQ.front() << "\n";
+            myQ.pop();
+        }
+    }
+    else if (s == "size"){
+        cout << myQ.size() << "\n";
+    }
+    else if (s == "empty"){
+        if(myQ.empty())
+            cout << 1 << "\n";
+        else
+            cout << 0 << "\n";
+    }
+    else if (s == "front"){
+        if (myQ.empty())
+            cout << -1 << "\n";
+        else
+        {
+            cout << myQ.front() << "\n";
+        }
+    }
+    else {
+        if (myQ.empty())
+            cout << -1 << "\n";
+        else
+        {
+            cout << myQ.back() << "\n";
+        }
+    }
 }
