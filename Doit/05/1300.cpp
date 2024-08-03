bba61411656f05a34e8be274b55bad524a03acc1
kimheera <hrk0295@naver.com>
2024-08-03 19:15:11 +0900

    1300.cpp 내가 생각한 접근 방법은 시간초과가 뜸. 책에 있는 방법으로 접근해야 함

--- Doit/05/1300.cpp
+++ Doit/05/1300.cpp
@@ -6,3 +6,18 @@ C++
 Last Changed: 2024.08.03
 */
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, k;
+    cin >> N >> k;
+
+    
+}
