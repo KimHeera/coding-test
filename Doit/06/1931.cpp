6f5714c636113e531400d58694719d0c229792c3
kimheera <hrk0295@naver.com>
2024-08-06 15:58:32 +0900

    1931.cpp 틀린 풀이

--- Doit/06/1931.cpp
+++ Doit/06/1931.cpp
@@ -6,3 +6,37 @@ C++
 Last Changed: 2024.08.06
 */
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> P(N);
+    for (int i = 0; i < N; i++){
+        cin >> P[i].first;
+        cin >> P[i].second;
+    }
+
+    sort(P.begin(), P.end());
+
+    int end = -1;
+    int count = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        if(end <= P[i].first){
+            end = P[i].second;
+            count++;
+        }
+    }
+
+    cout << count+1 << endl;
+}
