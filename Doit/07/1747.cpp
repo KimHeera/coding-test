6e20fcb6fedf0f7d48e51274036ca07d09fbf581
kimheera <hrk0295@naver.com>
2024-08-08 17:32:00 +0900

    1747.cpp 시간초과 뜸

---
+++ Doit/07/1747.cpp
@@ -0,0 +1,62 @@
+/*
+BOJ 1747.
+Author: Heera Kim
+E-mail Address: heeerala.a@gmail.com
+C++
+Last Changed: 2024.08.08
+*/
+
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+bool isPalindrome(int num);
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    long A[10000001];
+
+    for (int i = 2; i < 10000001; i++)
+        A[i] = i;
+
+    for (int i = 2; i <= sqrt(10000001); i++){
+        if(A[i] == 0)
+            continue;
+
+        for (int j = i + i; j < 10000001; j += i)
+            A[i] = 0;
+    }
+
+    int i = N;
+    while(true){
+        if(A[i] != 0){
+            if(isPalindrome(A[i])){
+                cout << A[i] << endl;
+                break;
+            }
+        }
+    }
+}
+
+bool isPalindrome(int num){
+    string input = to_string(num);
+    int start = 0;
+    int end = input.size() - 1;
+
+    while(start < end){
+        if(input[start] != input[end])
+            return false;
+
+        start++;
+        end--;
+    }
+
+    return true;
+}
