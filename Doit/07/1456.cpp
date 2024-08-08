8b6a885aa8c51fcacff0622765e30cdd00ccc604
kimheera <hrk0295@naver.com>
2024-08-08 15:30:52 +0900

    1456.cpp 풀이 시작

---
+++ Doit/07/1456.cpp
@@ -0,0 +1,20 @@
+/*
+BOJ 1456.
+Author: Heera Kim
+E-mail Address: heeerala.a@gmail.com
+C++
+Last Changed: 2024.08.08
+*/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    
+}
