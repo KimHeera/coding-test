b6c5f86f3a26d95160bdd2097ad0999d040dd0a8
kimheera <hrk0295@naver.com>
2024-08-08 15:20:47 +0900

    1929 풀이 완료

--- Doit/07/1929.cpp
+++ Doit/07/1929.cpp
@@ -8,6 +8,7 @@ Last Changed: 2024.08.08
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 int main(){
@@ -15,5 +16,25 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    
+    int M, N;
+    cin >> M >> N;
+
+    vector<int> primeNum(N + 1);
+
+    for (int i = 2; i <= N; i++){
+        primeNum[i] = i;
+    }
+
+    for (int i = 2; i <= sqrt(N); i++){
+        if(primeNum[i] == 0)
+            continue;
+
+        for (int j = i + i; j <= N; j += i)
+            primeNum[j] = 0;
+    }
+
+    for (int i = M; i <= N; i++){
+        if(primeNum[i] != 0)
+            cout << primeNum[i] << "\n";
+    }
 }
