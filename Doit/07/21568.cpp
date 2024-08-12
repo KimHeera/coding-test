534480c0a80c7ab3e85def044a1221a210e0f5bf
kimheera <hrk0295@naver.com>
2024-08-12 17:13:09 +0900

    21568.cpp 풀이 시작

---
+++ Doit/07/21568.cpp
@@ -0,0 +1,19 @@
+/*
+BOJ 21568.
+Author: Heera Kim
+E-mail Address: heeerala.a@gmail.com
+C++
+Last Changed: 2024.08.12
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    
+}
