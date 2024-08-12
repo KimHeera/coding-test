7b355b7d38ff69a37cb76dfcd6b55215212d983a
kimheera <hrk0295@naver.com>
2024-08-12 15:48:11 +0900

    1033.cpp 풀이 시작

--- Doit/07/1033.cpp
+++ Doit/07/1033.cpp
@@ -3,6 +3,24 @@ BOJ 1033.
 Author: Heera Kim
 E-mail Address: heeerala.a@gmail.com
 C++
-Last Changed: 2024.08.09
+Last Changed: 2024.08.12
 */
 
+#include <iostream>
+#include <vector>
+#include <tuple>
+using namespace std;
+
+int N;
+vector<tuple<int, int, int>> P[10];
+vector<bool> visited(10);
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+
+}
