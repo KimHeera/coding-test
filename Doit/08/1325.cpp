3e3e8132ffed78302392ebc1da0e86acffca2847
kimheera <hrk0295@naver.com>
2024-08-13 15:37:59 +0900

    1325.cpp 풀이 시작

---
+++ Doit/08/1325.cpp
@@ -0,0 +1,22 @@
+/*
+BOJ 1325.
+Author: Heera Kim
+E-mail Address: heeerala.a@gmail.com
+C++
+Last Changed: 2024.08.12
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    
+}
