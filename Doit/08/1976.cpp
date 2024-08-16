67d48b2b775806f7ca744b9952af4612b1efdaf6
kimheera <hrk0295@naver.com>
2024-08-16 17:24:30 +0900

    1976.cpp 풀이 시작

---
+++ Doit/08/1976.cpp
@@ -0,0 +1,19 @@
+/*
+BOJ 1976.
+Author: Heera Kim
+E-mail Address: heeerala.a@gmail.com
+C++
+Last Changed: 2024.08.16
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    
+}
