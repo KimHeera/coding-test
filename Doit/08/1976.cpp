2525f50a96ca433870e0ef5c33fc9c9d3fb6122e
kimheera <hrk0295@naver.com>
2024-08-16 17:55:20 +0900

    1976.cpp 예제는 통과. 제출시 틀림

--- Doit/08/1976.cpp
+++ Doit/08/1976.cpp
@@ -10,10 +10,67 @@ Last Changed: 2024.08.16
 #include <vector>
 using namespace std;
 
+void unionFunc(int a, int b);
+int find(int a);
+vector<int> P;
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    
+    int N, M;
+    cin >> N;
+
+    P.resize(N + 1);
+    for (int i = 1; i <= N; i++)
+        P[i] = i;
+
+    int dosi[201][201];
+    int path[1001];
+
+    for (int i = 1; i <= N; i++){
+        for (int j = 1; j <= N; j++){
+            cin >> dosi[i][j];
+        }
+    }
+
+    for (int i = 1; i <= M; i++){
+        cin >> path[i];
+    }
+
+    for (int i = 1; i <= N; i++){
+        for (int j = 1; j <= N; j++){
+            if(dosi[i][j] == 1)
+                unionFunc(i, j);
+        }
+    }
+
+    int index = find(path[1]);
+    bool connect = true;
+    for (int i = 2; i <= M; i++){
+        if(index != find(path[i])){
+            connect = false;
+            cout << "NO" << endl;
+            break;
+        }
+    }
+
+    if(connect)
+        cout << "YES" << endl;
+}
+
+void unionFunc(int a, int b){
+    a = find(a);
+    b = find(b);
+
+    if(a != b)
+        P[b] = a;
+}
+
+int find(int a){
+    if(a == P[a])
+        return a;
+    else
+        return P[a] = find(P[a]);
 }
