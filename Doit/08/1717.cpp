d7d29a2fa60a51255bd8929e9da0f34e589e3747
kimheera <hrk0295@naver.com>
2024-08-16 16:30:35 +0900

    1717.cpp 풀이 완료. 집합 문제 다양하게 풀어봐야 할듯

--- Doit/08/1717.cpp
+++ Doit/08/1717.cpp
@@ -6,4 +6,67 @@ C++
 Last Changed: 2024.08.16
 */
 
+#include <iostream>
+#include <vector>
+using namespace std;
 
+vector<int> P;
+void unionCal(int a, int b);
+int findCal(int a);
+bool isSame(int a, int b);
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+
+    P.resize(N + 1);
+    for (int i = 0; i <= N; i++){
+        P[i] = i;
+    }
+
+    int input, a, b;
+    while (M--){
+        cin >> input >> a >> b;
+
+        if(input == 0){
+            unionCal(a, b);
+        }
+        else{
+            findCal(a);
+
+            if(isSame(a, b))
+                cout << "YES" << "\n";
+            else
+                cout << "NO" << "\n";
+        }
+    }
+}
+
+void unionCal(int a, int b){
+    a = findCal(a);
+    b = findCal(b);
+
+    if(a != b)
+        P[b] = a;
+}
+
+int findCal(int a){
+    if(a == P[a])
+        return a;
+    else
+        return P[a] = findCal(P[a]);
+}
+
+bool isSame(int a, int b){
+    a = findCal(a);
+    b = findCal(b);
+
+    if(a == b)
+        return true;
+    else
+        return false;
+}
