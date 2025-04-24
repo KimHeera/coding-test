3ac1668fbcf7628e14c53bca6f2c0faeb61b7063
kimheera <hrk0295@naver.com>
2025-04-24 18:05:20 +0900

    1753 푸는중

--- Doit/08/1753.cpp
+++ Doit/08/1753.cpp
@@ -10,11 +10,23 @@ Last Changed: 2025.04.24
 #include <vector>
 using namespace std;
 
+vector<pair<int, int> > arr[20001];
+int dp[20001];
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int V, E;
+    int V, E, K;
+    cin >> V >> E;
+    cin >> K;
+
+    int u, v, w;
+    for (int i = 0; i < E; i++){
+        cin >> u >> v >> w;
+        arr[u].push_back(make_pair(v, w));
+    }
+
     
 }
