33361160540174be67c0f729bd512f8bd975ad44
kimheera <hrk0295@naver.com>
2025-04-29 18:18:26 +0900

    1753 풀이 완료. 한 번더 풀어보기

--- Doit/08/1753.cpp
+++ Doit/08/1753.cpp
@@ -7,8 +7,12 @@ Last Changed: 2025.04.24
 */
 
 #include <iostream>
+#include <cstring>
 #include <vector>
+#include <queue>
 using namespace std;
+
+#define INF 1000000
 
 vector<pair<int, int> > arr[20001];
 int dp[20001];
@@ -17,10 +21,10 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    memset(dp, INF, sizeof(dp));
 
     int V, E, K;
-    cin >> V >> E;
-    cin >> K;
+    cin >> V >> E >> K;
 
     int u, v, w;
     for (int i = 0; i < E; i++){
@@ -28,5 +32,46 @@ int main(){
         arr[u].push_back(make_pair(v, w));
     }
 
-    
+    dp[K] = 0;
+    priority_queue<pair<int, int> > pq;
+    pq.push(make_pair(0, K));
+
+    while(!pq.empty()){
+        int current = pq.top().second;
+
+        int start_to_current_distance = -pq.top().first;
+
+        pq.pop();
+
+        if (dp[current] < start_to_current_distance)
+            continue;
+
+        for (int i = 0; i < arr[current].size(); ++i){
+            int next = arr[current][i].first;
+            int start_to_next_distance = start_to_current_distance + arr[current][i].second;
+
+            if (dp[next] > start_to_next_distance){
+                dp[next] = start_to_next_distance;
+
+                pq.push(make_pair(-start_to_next_distance, next));
+            }
+        }
+    }
+
+
+    for (int i = 1; i < V + 1; ++i){
+        if (dp[i] > INF)
+            cout << "INF" << "\n";
+        else
+            cout << dp[i] << "\n";
+        
+    }
+
+    return 0;
 }
+
+
+
+/*
+pq의 first, second의 위치를 바꿔 실행하니 시간 초과 에러가 떴다. 왜일까?!?!!?
+*/
