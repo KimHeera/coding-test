af7b28662cf939a9f0702fba09cb6309fe1c8318
kimheera <hrk0295@naver.com>
2025-06-10 15:42:16 +0900

    11404.cpp 풀이 완료

--- Doit/08/11404.cpp
+++ Doit/08/11404.cpp
@@ -9,9 +9,9 @@ Last Changed: 2025.06.10
 #include <iostream>
 #include <vector>
 #include <limits.h>
+#include <math.h>
 using namespace std;
-// typedef pair<int, int> edge;
-vector<vector<int> > edges;
+vector<vector<long> > edges;
 
 int main(){
     ios::sync_with_stdio(false);
@@ -31,17 +31,25 @@ int main(){
     for (int i = 0; i < m; i++){
         cin >> a >> b >> c;
 
-        edges[a][b] = c;
+        if(edges[a][b] > c)
+            edges[a][b] = c;
     }
 
-    
+    for (int i = 1; i <= n; i++){
+        for (int s = 1; s <= n; s++){
+            for (int e = 1; e <= n; e++){
+                edges[s][e] = min(edges[s][e], edges[s][i] + edges[i][e]);
+            }
+        }
+    }
 
-    // for (int i = 1; i <= n; i++)
-    // {
-    //     for (int j = 1; j <= n; j++)
-    //     {
-    //         cout << edges[i][j] << "  ";
-    //     }
-    //     cout << "\n";
-    // }
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            if (edges[i][j] == INT_MAX)
+                cout << 0 << " ";
+            else
+                cout << edges[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }
