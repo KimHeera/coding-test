f4e7639ebe457a5163040db58c48181358d60387
Heerala <91535597+KimHeera@users.noreply.github.com>
2026-06-08 17:37:38 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.04 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+def solution(array):
+    counter = Counter(array)
+    answer = counter.most_common()
+    if len(answer) >= 2 and answer[0][1] == answer[1][1]:
+        return -1
+    return answer[0][0]
