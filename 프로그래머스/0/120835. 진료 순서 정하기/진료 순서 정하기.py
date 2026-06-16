3aff9716a15f5d8685fa8ec36c7f12c3222fef62
Heerala <91535597+KimHeera@users.noreply.github.com>
2026-06-16 22:21:48 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -0,0 +1,8 @@
+def solution(emergency):
+    answer = []
+    sort = sorted(emergency, reverse = True)
+    
+    for i in emergency:
+        answer.append(sort.index(i) + 1)
+    
+    return answer
