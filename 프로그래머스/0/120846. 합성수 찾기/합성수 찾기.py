7e7f002b08842a628881b6e8a524b7f0a11cc6eb
Heerala <91535597+KimHeera@users.noreply.github.com>
2026-06-25 23:09:52 +0900

    [level 0] Title: 합성수 찾기, Time: 0.16 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    
+    for i in range(n+1):
+        tmp = 0 
+        for j in range(1, i+1):
+            if (i % j == 0):
+                tmp +=1
+            
+        if tmp >= 3:
+            answer += 1
+                
+    return answer
