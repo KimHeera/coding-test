dc96cb39bb48c579cfc30f31c771f3c4f5f1c1f1
Heerala <91535597+KimHeera@users.noreply.github.com>
2026-07-13 23:49:03 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.04 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+#     inst = my_string.split()
+#     answer = int(inst[0])
+    
+#     if(inst[1] == "+"):
+#         answer += int(inst[2])
+#     else:
+#         answer -= int(inst[2])
+    return eval(my_string)
