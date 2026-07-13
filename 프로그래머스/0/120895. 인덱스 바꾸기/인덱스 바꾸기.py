47974d3505b93507338d1cb3d19e293066187f6a
Heerala <91535597+KimHeera@users.noreply.github.com>
2026-07-13 23:12:02 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -0,0 +1,9 @@
+def solution(my_string, num1, num2):
+    answer = list(my_string)
+    
+    tmp = answer[num1]
+    answer[num1] = answer[num2]
+    answer[num2] = tmp
+
+    answer = "".join(answer)
+    return answer
