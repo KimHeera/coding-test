fb1980b2fa77d0135f376cdbbb3de75e40007121
Heerala <91535597+KimHeera@users.noreply.github.com>
2026-07-13 23:49:56 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,9 +1,9 @@
 def solution(my_string):
-#     inst = my_string.split()
-#     answer = int(inst[0])
-    
-#     if(inst[1] == "+"):
-#         answer += int(inst[2])
-#     else:
-#         answer -= int(inst[2])
-    return eval(my_string)
+    inst = my_string.split()
+    answer = int(inst[0])
+    for i in range(1, len(inst), 2):  # 연산자는 홀수 인덱스
+        if inst[i] == "+":
+            answer += int(inst[i+1])
+        else:
+            answer -= int(inst[i+1])
+    return answer
